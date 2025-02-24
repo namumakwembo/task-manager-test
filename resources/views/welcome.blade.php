2b2199a9bbd8bb74e94e7dbfa2a40981e0744736
Namu Makwembo <makwembonamu@gmail.com>
2025-02-24 22:18:35 +0300

    fixed: missing route in dashboard:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
                         <nav class=" ">
                             @auth
                                 <a
-                                    href="{{ url('/filament.app.pages.dashboard') }}"
+                                    href="{{ route('filament.app.pages.dashboard') }}"
                                     class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                 >
                                     Dashboard
