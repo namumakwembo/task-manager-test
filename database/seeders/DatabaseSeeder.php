146c79615ccef083a97617f63a5b648dfb3e3794
Namu Makwembo <makwembonamu@gmail.com>
2025-02-24 21:53:57 +0300

    updated:readme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,16 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        User::factory()->create([
+       $test= User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
 
+        \App\Models\Task::factory()->count(20)->create(['user_id' => $test->id]);
+
             // Create 10 users and assign 5 tasks to each
     \App\Models\User::factory(10)->create()->each(function ($user) {
-        \App\Models\Task::factory()->count(5)->create(['user_id' => $user->id]);
+        \App\Models\Task::factory()->count(20)->create(['user_id' => $user->id]);
     });
     }
 }
