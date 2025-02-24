c151669ae42f0e9d04bd21ecfcea02a47a3cb9b2
Namu Makwembo <makwembonamu@gmail.com>
2025-02-24 22:09:53 +0300

    added:register routes

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -30,6 +30,7 @@ class AppPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Teal,
             ])
+            ->spa()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
