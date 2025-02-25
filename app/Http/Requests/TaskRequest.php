ef027b0557f81de4068e929ef6e8b5ae66bb26ac
Namu Makwembo <makwembonamu@gmail.com>
2025-02-25 13:48:47 +0300

    added validations to TaskRequest

--- app/Http/Requests/TaskRequest.php
+++ app/Http/Requests/TaskRequest.php
@@ -11,7 +11,7 @@ class TaskRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class TaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'status' => ['required', 'in:pending,completed'],
         ];
     }
 }
